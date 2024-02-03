ad63c480f4a319fa1ec4aed9a00249bb26967d84
Samartha Madhyastha <samartha.m3@gmail.com>
2024-02-03 15:50:25 -0500

    HTML-parsed data
    Resolved issues with multi-line string table by using html parser

---
+++ reader.py
@@ -0,0 +1,30 @@
+from html.parser import HTMLParser
+
+class reader(HTMLParser):
+    hasData = False
+    relevance = False
+    Table = list()
+    row = list()
+    
+    def handle_starttag(self, tag, attrs):
+        match tag:
+            case "table":
+                print(attrs)
+                self.hasData = True
+            case "tr":
+                self.row = list()
+            case "td":
+                self.relevance = True
+    def handle_endtag(self, tag):
+        match tag:
+            case "tr":
+                self.Table.append(self.row)
+            case "td":
+                self.relevance=False
+    def handle_data(self, data):
+        if self.relevance == True:
+            self.row.append(data)
+    def getRow(self):
+        return self.row
+    def getData(self):
+        return self.Table
