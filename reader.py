07d9581ac878b302ce8751079c13453318d63bdc
Samartha Madhyastha <samartha.m3@gmail.com>
2024-02-03 18:50:15 -0500

    Historic-data
    Developed class to retrieve historic tick data for several days.

--- reader.py
+++ reader.py
@@ -15,12 +15,16 @@ class reader(HTMLParser):
                 self.row = list()
             case "td":
                 self.relevance = True
+            case "th":
+                self.relevance = True
     def handle_endtag(self, tag):
         match tag:
             case "tr":
                 self.Table.append(self.row)
             case "td":
-                self.relevance=False
+                self.relevance = False
+            case "th":
+                self.relevance = False
     def handle_data(self, data):
         if self.relevance == True:
             self.row.append(data)
